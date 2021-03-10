326ef1019ff22c8c8a3b2e7ef329eb11161527be
Dhruv Maniyaa <71778799+midnightmadwalk@users.noreply.github.com>
2021-03-10 23:18:01 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,6 @@ from .autopinger import ping
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 scheduler = AsyncIOScheduler()
-scheduler.add_job(ping, "interval", seconds=600)
+scheduler.add_job(ping, "interval", seconds=20)
 
 scheduler.start()
